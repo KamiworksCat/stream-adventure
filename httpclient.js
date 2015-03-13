b922d9edc0a0eb074828e5358672c00425e09894
CatCoder <waikeat@kamiworks.com>
2015-03-13 16:30:50 +0800

    New files to add

---
+++ httpclient.js
@@ -0,0 +1,5 @@
+request = require('request')
+
+r = request.post('http://localhost:8099')
+
+process.stdin.pipe(r).pipe(process.stdout)
---
+++ websockets.js
@@ -0,0 +1,5 @@
+ws = require('websocket-stream')
+
+stream = ws('ws://localhost:8099')
+
+stream.write('hello\n')
