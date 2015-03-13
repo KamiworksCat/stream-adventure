ba1515c32e32126821699459f62081e22a8ba8c1
CatCoder <waikeat@kamiworks.com>
2015-03-13 12:41:55 +0800

    Added lines.js

--- lines.js
+++ lines.js
@@ -1,2 +1,45 @@
 split = require('split')
-through = require('through2')
+through = require('through')
+
+alt = function (oddnum, evennum){
+	f = evennum
+	return function (buffer){
+		f = (f === evennum) ? oddnum : evennum
+		f.call(this, buffer)
+	}
+}
+
+switchBoard = function (stringnum){
+	return function (buffer){
+		value = stringnum.call(buffer.toString()) + '\n'
+		this.queue(value)
+	}
+}
+
+process.stdin
+	.pipe(split())
+	.pipe(through(alt(
+		switchBoard(String.prototype.toLowerCase),
+		switchBoard(String.prototype.toUpperCase))))
+	.pipe(process.stdout)
+
+/* Official Solution
+var through = require('through2');
+var split = require('split');
+
+var lineCount = 0;
+var tr = through(function (buf, _, next) {
+    var line = buf.toString();
+    this.push(lineCount % 2 === 0
+        ? line.toLowerCase() + '\n'
+        : line.toUpperCase() + '\n'
+    );
+    lineCount ++;
+    next();
+});
+process.stdin
+    .pipe(split())
+    .pipe(tr)
+    .pipe(process.stdout)
+;
+*/
